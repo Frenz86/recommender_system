22913f091bf9f4a063d778568cc1997be8bf5836
Frenz86 <frenzy86@gmail.com>
2021-01-13 14:05:47 +0100

    ww

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = '4891a20e43ff173c0549d0c337d9bd73';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = '4891a20e43ff173c0549d0c337d9bd73';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
